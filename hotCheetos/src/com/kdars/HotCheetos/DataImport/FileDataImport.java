e4aac1a1658e14ae543b7415c029ddd6cb66e9c6
Jin <Jin@Jin-PC>
2015-02-08 12:32:23 -0500

    working on unzip. Still working.

--- hotCheetos/src/com/kdars/HotCheetos/DataImport/FileDataImport.java
+++ hotCheetos/src/com/kdars/HotCheetos/DataImport/FileDataImport.java
@@ -3,10 +3,14 @@ package com.kdars.HotCheetos.DataImport;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 
 public class FileDataImport implements ImportContent {
@@ -46,8 +50,45 @@ public class FileDataImport implements ImportContent {
 		return content;
 	}
 	
-	public void receiveFile(){
-		
+	public void unZipAndSaveZipFile(File zipFile) {
+		byte[] buffer = new byte[1024];
+		String outputFolder = "D:\\UnzipTestComplete";
+
+		try {
+
+			// create output directory is not exists
+			File folder = new File(outputFolder);
+			if (!folder.exists()) {
+				folder.mkdir();
+			}
+
+			// get the zip file content
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+			// get the zipped file list entry
+			ZipEntry ze = zis.getNextEntry();
+
+			while (ze != null) {
+				String fileName = ze.getName();
+				File newFile = new File(outputFolder + File.separator + fileName);
+
+				new File(newFile.getParent()).mkdirs();
+
+				FileOutputStream fos = new FileOutputStream(newFile);
+
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+
+				fos.close();
+				ze = zis.getNextEntry();
+			}
+
+			zis.closeEntry();
+			zis.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	public String extractOnlyText(String text){
