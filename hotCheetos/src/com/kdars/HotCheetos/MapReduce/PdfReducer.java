a1ed157ddb3933690949bc0f26e4987f71adcb9a
Jin <Jin@Jin-PC>
2015-03-01 21:06:30 -0500

    Reducer implemented.  Need to test!

--- hotCheetos/src/com/kdars/HotCheetos/MapReduce/PdfReducer.java
+++ hotCheetos/src/com/kdars/HotCheetos/MapReduce/PdfReducer.java
@@ -1,23 +1,54 @@
 package com.kdars.HotCheetos.MapReduce;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import com.kdars.HotCheetos.DB.DBManager;
-import com.kdars.HotCheetos.Parsing.Parse1_nGram_hashcode;
+import com.kdars.HotCheetos.DocumentStructure.DocumentInfo;
+import com.kdars.HotCheetos.Parsing.NGram_hashcode_mapReduce;
 
 public class PdfReducer extends Reducer<Text, Text, Text, IntWritable>{
 	
 	@Override
-	public void reduce(Text title, Iterable<Text> content, Context context){
+	public void reduce(Text title, Iterable<Text> contents, Context context) throws IOException, InterruptedException {
 		
-		for (Text text : content){ //어차피 mapper에서는 한 key 당 하나의 value(pdf 내용)을 넘겨주기 때문에 iterate은 한번 밖에 안하게 될 것.
-			DBManager.getInstance().insertRowToTextTable(title.toString(), text.toString());
+		int docID = 0;
+		String content = null;
+		
+		int contentCount = 0; //어차피 mapper에서는 한 key 당 하나의 value(pdf 내용)을 넘겨주기 때문에 iterate은 한번 밖에 안하게 되는게 정상.
+		for (Text text : contents){
+			
+			contentCount++;
+			
+			content = text.toString();
+			
+			//여기서 text table에 저장 후 docID 가져옴.
+			docID = DBManager.getInstance().insertRowAndGetDocIDArray(title.toString(), text.toString());  //결국 하나 밖에 안 담음.
 		}
 		
-		Parse1_nGram_hashcode test = new Parse1_nGram_hashcode();
+		if (contentCount != 1){
+			//content가 여러개 들어왔다?? 한 제목당??  이건 비정상.
+			System.out.println("Reducer에서 고장났음");
+		}
 		
+		NGram_hashcode_mapReduce test = new NGram_hashcode_mapReduce();
+		
+		//여기서 inverted index table에 저장함.
+		//일단 test를 위해 default로 3-gram hashcode fingerprint 진행하게 됨.
+		DocumentInfo docInfo = test.parseDoc(content, docID, 73);  // 여기서 73은 table id로 일단 sentence-hashcode table에 저장하게 됨.
+		
+		for(String key : docInfo.termFreq.keySet()){
+			Text keyText = new Text();
+			keyText.set(key);
+			
+			IntWritable valueInt = new IntWritable();
+			valueInt.set(docInfo.termFreq.get(key));
+			
+			context.write(keyText, valueInt);
+		}
 		
 	}
 }
---
+++ hotCheetos/src/com/kdars/HotCheetos/Parsing/NGram_hashcode_mapReduce.java
@@ -0,0 +1,115 @@
+package com.kdars.HotCheetos.Parsing;
+
+import java.util.ArrayList;
+
+import com.kdars.HotCheetos.Config.Configuration;
+import com.kdars.HotCheetos.DB.DBManager;
+import com.kdars.HotCheetos.DocumentStructure.DocumentInfo;
+
+public class NGram_hashcode_mapReduce{
+	
+	private ArrayList<Integer> stopwordHashList = new ArrayList<Integer>();
+	private ArrayList<String> stopWordList = DBManager.getInstance().getStopwords();
+	/*Temporary measure for experiment.  Need to delete!!!! */
+	private int nGramSetting = Configuration.getInstance().getNgramSetting();
+	private int fingerprintSetting = Configuration.getInstance().getFingerprintSetting();
+	/*Temporary measure for experiment.  Need to delete!!!! */
+	
+	public NGram_hashcode_mapReduce(){
+		for (String stopword : stopWordList){
+			stopwordHashList.add(stopword.hashCode());
+		}
+	}
+	
+	public DocumentInfo parseDoc(String content, int documentID, int invertedIndexTableID) {
+		DocumentInfo docInfo = new DocumentInfo();
+		docInfo.docID = documentID;
+		
+		char wholeChar[] = content.toCharArray();
+		
+		ArrayList<Integer> nGramMaker = new ArrayList<Integer>();
+		int ngramMaker = 0;
+		
+//		int lastIndexOfnonBlankCharacter = 0;
+		int hashCharSum = 0;
+		for (int i = 0; i < wholeChar.length; i++){
+			
+			//whitespace가 아닌 캐릭터는 해쉬코드 더함.
+			if (wholeChar[i] != ' '){
+				hashCharSum += wholeChar[i];
+//				lastIndexOfnonBlankCharacter = i;
+				
+				//문장의 마지막 단어 처리.
+				if (i != wholeChar.length - 1){
+					continue;
+				}
+			}
+			
+			//연속으로 whitespace일 경우에는 그 다음 포문 탐.
+			if (hashCharSum == 0){
+				continue;
+			}
+			
+//			//한 글자짜리는 n-gram으로 안치고 그 다음 포문 탐.
+//			if ((i >= 2 && wholeChar[i-2] == ' ') || i < 2){
+//				nGramMaker.clear();
+//				ngramMaker = 0;
+//				hashCharSum = 0;
+//				continue;
+//			}
+			
+			//아무 전처리 없는 상태에서 주어진 단어가 stopword list에 포함된 단어라면 그 다음 포문 탐.
+			if (stopwordHashList.contains(hashCharSum)){
+				hashCharSum = 0;
+				continue;
+			}
+			
+			//nGramMaker arrayList에 parameter로 받은 n-gram 갯수만큼의 hashcode가 차면 hashcode를 더해서 hashmap 만들고, 0번째 hashcode를 버림으로써 다음 ngram 만들 준비. 
+			nGramMaker.add(hashCharSum);
+			ngramMaker += hashCharSum;
+			
+//			//마지막으로 더한 ngramComponent의 마지막 캐릭터가 마침표일 경우에는 nGramMaker를 비우고 처음부터 다시 ngram 만듬.
+//			char period = '.';
+//			if (wholeChar[lastIndexOfnonBlankCharacter] == '.'){
+//				ngramMaker -= period;
+//				if (nGramMaker.size() == this.nGramSetting){
+//					docInfo = addHash(docInfo, ngramMaker);
+//				}
+//				nGramMaker.clear();
+//				ngramMaker = 0;
+//				hashCharSum = 0;
+//				continue;
+//			}
+			
+			//nGramMaker arrayList에 parameter로 받은 n-gram 갯수만큼의 hashcode가 차면 hashcode를 더해서 hashmap 만들고, 0번째 hashcode를 버림으로써 다음 ngram 만들 준비.
+			if (nGramMaker.size() == this.nGramSetting){
+				addHash(docInfo, ngramMaker);
+				ngramMaker -= nGramMaker.get(0);
+				nGramMaker.remove(0);
+			}
+			
+			//whitespace가 detect되고 한글자 짜리 단어가 아니라면, 새로운 ngramComponent를 만들기 위해 hashCharSum 리셋함. 
+			hashCharSum = 0;
+			
+		}
+		
+		if(!DBManager.getInstance().insertBulkToHashTable(docInfo, invertedIndexTableID)){
+			System.out.println("parse된 hashmap을 DB에 저장 실패했음");
+		}
+		
+		return docInfo;
+	}
+	
+	private void addHash(DocumentInfo docInfo, int hash) {
+		if (hash % this.fingerprintSetting != 0) {
+			return;
+		}
+		String hashToString = String.valueOf(hash);
+		if (docInfo.termFreq.containsKey(hashToString)) {
+			int value = docInfo.termFreq.get(hashToString);
+			docInfo.termFreq.replace(hashToString, value + 1);
+			return;
+		}
+		docInfo.termFreq.put(hashToString, 1);
+	}
+}
